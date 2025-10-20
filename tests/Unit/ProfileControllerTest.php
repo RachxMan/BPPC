53448b0bf75009348b70464c1db071fd261a59e5
Mutia Rizkianti Ruslan <158070732+mootee4@users.noreply.github.com>
2025-10-20 23:13:12 +0700

    Fix: Update ProfileControllerTest to match /profil route

--- tests/Unit/ProfileControllerTest.php
+++ tests/Unit/ProfileControllerTest.php
@@ -9,7 +9,8 @@ class ProfileControllerTest extends TestCase
     /** @test */
     public function it_can_render_profile_view()
     {
-        $response = $this->get('/profile');
+        // Uji apakah halaman profil bisa diakses
+        $response = $this->get('/profil');
 
         $response->assertStatus(200);
         $response->assertViewIs('profil_pengaturan');
@@ -18,13 +19,14 @@ class ProfileControllerTest extends TestCase
     /** @test */
     public function it_can_update_profile_and_redirect_back()
     {
-        $response = $this->post('/profile/update', [
+        // Uji apakah form update profil bisa dijalankan dan redirect kembali
+        $response = $this->post('/profil/update', [
             'first_name' => 'Aya',
             'last_name' => 'Test',
             'email' => 'aya@example.com',
             'mobile' => '08123456789',
             'gender' => 'Perempuan',
-            'address' => 'Pekanbaru'
+            'address' => 'Pekanbaru',
         ]);
 
         $response->assertRedirect(route('profile.index'));
