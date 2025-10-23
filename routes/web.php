9dbcad4502241859747d1fe337dce7791b90f6dc
Amanda H <amandafebrinaputri@gmail.com>
2025-10-23 12:59:51 +0700

    Resolve merge conflict in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\KelolaController;
 
-Route::get('/', function () {
-    return view('login');
-})->name('home');
+Route::get('/', fn() => redirect()->route('login'))->name('home');
 
 Route::get('/login', function () {
     return view('login');
@@ -30,31 +28,31 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::get('/mailing-list', function () {
-    return view('mailing_list');
-})->name('mailing.index');
+// Middleware khusus admin
+Route::middleware(['role:admin'])->group(function () {
+    // Upload Data & Mailing List
+    Route::get('/upload-data', [UploadDataController::class, 'index'])->name('upload.index');
+    Route::get('/mailing-list', [MailingListController::class, 'index'])->name('mailing.index');
 
-Route::get('/upload-data', function () {
-    return view('upload_data');
-})->name('upload.index');
+    // Laporan
+    Route::prefix('laporan')->group(function () {
+        Route::get('/harian', [ReportController::class, 'harian'])->name('report.harian');
+        Route::get('/bulanan', [ReportController::class, 'bulanan'])->name('report.bulanan');
+    });
 
-Route::prefix('laporan')->group(function () {
-    Route::get('/harian', [ReportController::class, 'harian'])->name('report.harian');
-    Route::get('/bulanan', [ReportController::class, 'bulanan'])->name('report.bulanan');
+    // Halaman Kelola Akun
+    Route::prefix('kelola-akun')->name('kelola.')->group(function () {
+        Route::get('/', [KelolaController::class, 'index'])->name('index');
+        Route::get('/create', [KelolaController::class, 'create'])->name('create');
+        Route::post('/', [KelolaController::class, 'store'])->name('store');
+        Route::get('/{user}/edit', [KelolaController::class, 'edit'])->name('edit');
+        Route::put('/{user}', [KelolaController::class, 'update'])->name('update');
+        Route::delete('/{user}', [KelolaController::class, 'destroy'])->name('destroy');
+        Route::patch('/{user}/toggle', [KelolaController::class, 'toggleStatus'])->name('toggle');
+    });
 });
 
-// Halaman Kelola Akun
-Route::prefix('kelola-akun')->name('kelola.')->group(function () {
-    Route::get('/', [KelolaController::class, 'index'])->name('index');
-    Route::get('/create', [KelolaController::class, 'create'])->name('create');
-    Route::post('/', [KelolaController::class, 'store'])->name('store');
-    Route::get('/{user}/edit', [KelolaController::class, 'edit'])->name('edit');
-    Route::put('/{user}', [KelolaController::class, 'update'])->name('update');
-    Route::delete('/{user}', [KelolaController::class, 'destroy'])->name('destroy');
-    Route::patch('/{user}/toggle', [KelolaController::class, 'toggleStatus'])->name('toggle');
-});
-
-
+// Profil
 Route::get('/profil', function () {
     return view('profil_pengaturan');
 })->name('profile.index');
