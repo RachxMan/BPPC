21e271b59a202c7f90ac6395e858481ec8fe016c
Mutia Rizkianti Ruslan <158070732+mootee4@users.noreply.github.com>
2025-10-23 19:48:07 +0700

    update halaman upload akun

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MailingListController;
-use App\Http\Controllers\UploadDataController;
+use App\Http\Controllers\Admin\UploadDataController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\KelolaController;
