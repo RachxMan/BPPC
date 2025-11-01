27a19ec6707a60e646b467d874d5376cff02ffdb
RachxMan <heisha.rachman3911@student.unri.ac.id>
2025-11-02 01:03:38 +0700

    Create detail-pelanggan.blade.php

---
+++ resources/views/admin/dashboard/detail-pelanggan.blade.php
@@ -0,0 +1,28 @@
+{{-- Pop-up for Detail Pelanggan --}}
+<div class="popup-content">
+    <h3>Detail Pelanggan</h3>
+    <table class="interactive-table">
+        <thead>
+            <tr>
+                <th>ID NET</th>
+                <th>NAMA</th>
+                <th>ALAMAT</th>
+                <th>KONTAK</th>
+                <th>STATUS</th>
+                <th>TANGGAL PEMBAYARAN</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($dataPelanggan as $pel)
+            <tr>
+                <td>{{ $pel->snd }}</td>
+                <td>{{ $pel->nama }}</td>
+                <td>{{ $pel->datel }}</td>
+                <td>{{ $pel->cp ?? $pel->no_hp }}</td>
+                <td>{{ $pel->status_bayar }}</td>
+                <td>{{ $pel->payment_date ?? '-' }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
